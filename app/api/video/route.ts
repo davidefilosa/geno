878a3c30d087b1f1be3f7fb65cb5f9d54150f691
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2023-09-27 13:16:45 +0200

    Update route.ts

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
-const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN });
+const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN! });
 
 export async function POST(req: Request) {
   try {
