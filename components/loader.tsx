d643e9206be34cd2aa052ceaa88431d1933eb04d
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2023-09-27 10:52:09 +0200

    Update loader.tsx

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-8 h-8 relative animate-bounce rounded-full">
+      <div className="w-8 h-8 relative animate-bounce rounded-full overflow-hidden">
         <Image alt="Logo" src="/avatar.png" fill />
       </div>
       <p className="text-sm text-muted-foreground">Genō is thinking...</p>
