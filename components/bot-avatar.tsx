2f36f0a43f494faa183eb1eb04155cb1a6b3b58f
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2023-09-27 10:48:01 +0200

    Update bot-avatar.tsx

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const BotAvatar = () => {
   return (
-    <Avatar className="w-8 h-8">
+    <Avatar className="w-8 h-8 rounded-full">
       <AvatarImage src="/avatar.png" />
     </Avatar>
   );
