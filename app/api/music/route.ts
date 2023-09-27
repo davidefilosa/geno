b12766cb13b72a513abfef2e948604eab91caaa4
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2023-09-27 13:12:40 +0200

    Update route.ts

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,31 +1,29 @@
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
-const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN! });
+const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN });
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { prompt } = body;
 
-   
-
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
 
     const response = await replicate.run(
-      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
       {
         input: {
-          prompt_a: prompt,
+          prompt,
         },
       }
     );
 
     return NextResponse.json(response);
   } catch (error) {
-    console.log("[MUSIC_ERROR]", error);
+    console.log("[VIDEO_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
